f963a5971d8308988b8c8cc9d84e73b6b40eaaea
Ragav Endiran <srm482@gmail.com>
2024-01-02 09:27:33 +0530

    [DAY-17] added edit case

--- Todo App/gui.py
+++ Todo App/gui.py
@@ -5,9 +5,10 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter the todo", key="todo")
 add_btn = sg.Button("Add")
+edit_btn = sg.Button("Edit")
 
 window = sg.Window("My TO-DO-APP",
-                   layout=[[label], [input_box, add_btn]],
+                   layout=[[label],[input_box, add_btn],[edit_btn]],
                    font=('Helvetica', 20))
 
 while True:
@@ -20,6 +21,9 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+        case "Edit":
+            pass
         case sg.WIN_CLOSED:
             break
+
 window.close()
