45417e6e0b8c0261593592602f6a4e1fefcd248d
Ragav Endiran <srm482@gmail.com>
2024-01-02 09:11:25 +0530

    [DAY-17] added key to lable which refelect on event output

--- Todo App/gui.py
+++ Todo App/gui.py
@@ -3,11 +3,17 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter the todo")
+input_box = sg.InputText(tooltip="Enter the todo", key="todo")
 add_btn = sg.Button("Add")
 
 window = sg.Window("My TO-DO-APP",
                    layout=[[[label], [input_box, add_btn]]],
                    font=('Helvetica', 20))
-window.read()
+event = window.read()
+print("Hi, event", event)
+
+
+
+
+
 window.close()
