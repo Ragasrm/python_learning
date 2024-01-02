a2c9a8e1343e3ad84541f95ac0e7e6c27743da5a
Ragav Endiran <srm482@gmail.com>
2024-01-02 09:07:19 +0530

    [DAY-17] added font family and size to GUI

--- Todo App/gui.py
+++ Todo App/gui.py
@@ -6,6 +6,8 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter the todo")
 add_btn = sg.Button("Add")
 
-window = sg.Window("My TO-DO-APP", layout=[[[label], [input_box, add_btn]]])
+window = sg.Window("My TO-DO-APP",
+                   layout=[[[label], [input_box, add_btn]]],
+                   font=('Helvetica', 20))
 window.read()
 window.close()
