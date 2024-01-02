c33481d9071b1c52a51033a51a3dfca6515801a8
Ragav Endiran <srm482@gmail.com>
2024-01-02 09:23:20 +0530

    [DAY-17] added GUI to add todos in txt file

--- Todo App/gui.py
+++ Todo App/gui.py
@@ -7,13 +7,17 @@ input_box = sg.InputText(tooltip="Enter the todo", key="todo")
 add_btn = sg.Button("Add")
 
 window = sg.Window("My TO-DO-APP",
-                   layout=[[[label], [input_box, add_btn]]],
+                   layout=[[label], [input_box, add_btn]],
                    font=('Helvetica', 20))
-event = window.read()
-print("Hi, event", event)
 
-
-
-
-
+while True:
+    event, values = window.read()
+    print("Hi, event", event)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            print("todos", todos)
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
 window.close()
